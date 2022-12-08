9db18be1c03f6e2131cf415c05905bb89557bc25
JalalMitali <JalalMitaliShopping@gmail.com>
2022-12-08 13:54:00 +0100

    deploy

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,6 +1,3 @@
-const path = require('path')
-require('dotenv').config({path: path.resolve(process.cwd() + '/.env.local'), debug: false})
-
 export default {
     mongoURI: process.env.MONGO_URI
 }
--- src/payments/constants.ts
+++ src/payments/constants.ts
@@ -1,5 +1,2 @@
-const fs = require('fs');
-const path = require('path')
-require('dotenv').config({ path: path.resolve(process.cwd() + "/.env.local") })
 
 export const stripe = process.env.STRIPE_SECRET_KEY
